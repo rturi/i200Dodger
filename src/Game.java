cd7c618a178e82d6608f46bd44ded0ec5332ac1b
rturi <rolandtyri@gmail.com>
2015-10-26 22:42:32 +0200

    generate board, fill it with random obstacles

--- src/Game.java
+++ src/Game.java
@@ -10,11 +10,17 @@ public class Game {
 
     public static void main(String[] args){
 
-        Obstacle obstacle = new Obstacle();
+        GameBoard gameboard = new GameBoard();
 
-        obstacle.setColor("vermilion");
+        gameboard.initiateBoard();
 
-        System.out.println(obstacle.getColor());
+        for (int i = 0; i < 6; i++) {
+
+            gameboard.insertRow();
+            System.out.println("uus laua seis");
+            gameboard.printBoard();
+            System.out.println();
+        }
 
     }
 
@@ -22,8 +28,8 @@ public class Game {
 
         String color = "unset";
 
-        public void setColor(String color) {
-            this.color = color;
+        public void setColor(String inputColor) {
+            color = inputColor;
         }
 
         public String getColor() {
@@ -35,7 +41,54 @@ public class Game {
 
     public static class GameBoard{
 
+        Obstacle[][] board = new Obstacle[6][3];
 
+
+        public void initiateBoard(){
+
+            for (int i = 0; i < board.length ; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    board[i][j] = new Obstacle();
+                    board[i][j].setColor("____");
+                }
+            }
+
+
+        }
+
+        public void printBoard (){
+
+            for (int i = 0; i < board.length ; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    System.out.print(board[i][j].getColor() + " ");
+                }
+                System.out.println();
+            }
+
+        }
+
+        public void insertRow (){
+
+            //first shift all existing objects one row down
+
+            for (int i = board.length - 1; i > 0  ; i--) {
+                for (int j = 0; j < board[0].length; j++) {
+                    board[i][j].setColor(board[i-1][j].getColor());
+                }
+
+            }
+
+            //insert new row
+
+            for (int i = 0; i < board[0].length; i++) {
+
+                if (((int) (Math.random()*2) == 0))
+                    board[0][i].setColor("blue");
+                else
+                    board[0][i].setColor("red ");
+            }
+
+        }
     }
 
 
